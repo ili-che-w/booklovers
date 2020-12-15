a9fa2c1099a3665e781075a6b6418d8a245ff4ba
Wanja Ilichev <john.the.theologician@gmail.com>
2020-12-15 11:46:00 +0300

    finalize basics

--- app/config.py
+++ app/config.py
@@ -1,2 +1,7 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql:///booklovers'
+import os
+
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://booklovers:d3r_pAr0l@localhost/booklovers'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+FLASK_ENV = 'development'
+FLASK_DEBUG = True
